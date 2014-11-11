dc8a176ba232feb674ed9b564e2ad94d7601091b
JordanKoski <jordan_koski@asl.oeg>
2014-11-11 15:02:48 +0000

    Lab 05

--- Lab05.java
+++ Lab05.java
@@ -2,16 +2,35 @@
 *
 * Description of the program goes here  // provide a brief description
 *
-* @author <Your Name here>  // replace <...> with your name
-* @version <date/of/completion> // replace <...> with the date
+* @author <Jordan Koski>  // replace <...> with your name
+* @version <November 7th> // replace <...> with the date
 */
 
 import edu.fcps.karel2.Display;
 import edu.fcps.karel2.Robot;
-
-public class Lab05 {
-
-     public static void main(String[] args) {
-          // TODO Your code goes here
+ 
+ public class Lab05 {
+ 
+      public static void runTheRace(Racer arg) {
+         arg.shuttle(4, 7);
+         arg.shuttle(6, 5);
+         arg.shuttle(8, 3);
+         arg.move();
      }
+ 
+      public static void main(String[] args) {
+     
+     Display.openWorld("maps/shuttle.map");
+     Display.setSize(10,10);
+     Display.setSpeed(15);            
+            
+            Racer ava = new Racer(7);
+            Racer bob = new Racer(4);
+            Racer cam = new Racer(1);
+            runTheRace(ava);
+            runTheRace(bob);
+            runTheRace(cam);
+                 
+   }
+            
 }
--- Racer.java
+++ Racer.java
@@ -1,12 +1,84 @@
 /**
 *  <Describe that the Racer class does>
 *
-*  @author <your name here, no brackets>
-*  @version <version or date, no brackets>
+*  @author <Jordan Koski>
+*  @version <November 7th>
 */
 
-public class Racer extends Athlete {
+import edu.fcps.karel2.Robot;
+import edu.fcps.karel2.Display;
 
-// consult the Karel API to determine what constructors and methods this class should implement
-
-}
+public class Racer extends Athlete 
+   {
+    
+    public Racer (int y)
+     {
+      
+       super(1, y, Display.EAST, Display.INFINITY);
+     
+     }
+     
+    public void jumpRight()
+      {
+       
+       turnLeft();
+       move();
+       turnRight();
+       move();
+       turnRight();
+       move();
+       turnLeft();
+      
+      }
+      
+    public void jumpLeft()
+      {
+       
+       turnRight();
+       move();
+       turnLeft();
+       move();
+       turnLeft();
+       move();
+       turnRight();
+      
+      }
+      
+    public void sprint(int n)
+      {
+         
+         for(int k = 1; k <= n; k++)
+            move();
+      
+      }
+      
+    public void put(int n)
+      {
+       
+       for(int k = 1; k <= n; k++)
+            putBeeper();
+      }
+      
+    public void pick(int n)
+      {
+       
+       for(int k = 1; k <= n; k++)
+            pickBeeper();
+      }
+      
+    public void shuttle(int spaces, int beepers)
+      {
+       
+       move();
+       jumpRight();
+       sprint(spaces - 2);
+       pick(beepers);
+       turnAround();
+       sprint(spaces - 2);
+       jumpLeft();
+       move();
+       put(beepers);
+       turnAround();
+      
+      }
+    }
